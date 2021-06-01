f68dce1bbd4402af0de15b687007a332025f2c8c
nasty23-star <71438930+nasty23-star@users.noreply.github.com>
2021-06-01 11:53:56 +0300

    Add files via upload

---
+++ jquery_toggle_table.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+    $("#tableHead").click(function(){
+      $("#tableBody").slideToggle("slow");
+    });
+  });
+
+  
---
+++ js_search_table.js
@@ -0,0 +1,19 @@
+function tableSearch() {
+    var phrase = document.getElementById('search');
+    var table = document.getElementById('mytable');
+    var regPhrase = new RegExp(phrase.value, 'i');
+    var flag = false;
+    for (var i = 1; i < table.rows.length; i++) {
+        flag = false;
+        for (var j = table.rows[i].cells.length - 1; j >= 0; j--) {
+            flag = regPhrase.test(table.rows[i].cells[j].innerHTML);
+            if (flag) break;
+        }
+        if (flag) {
+            table.rows[i].style.display = "";
+        } else {
+            table.rows[i].style.display = "none";
+        }
+
+    }
+}
